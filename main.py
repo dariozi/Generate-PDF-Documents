283df198d8d5bd6244f807740f2cec012746bbf5
Dario Z <dario.zignale@yahoo.com>
2023-10-01 13:44:22 +0100

    lines in pages

--- main.py
+++ main.py
@@ -12,7 +12,10 @@ for index, row in df.iterrows():
     pdf.set_font(family="Times", style="B", size=24)
     pdf.set_text_color(100,100,100)
     pdf.cell(w=0, h=12, txt=row["Topic"], align="Left", ln=1)
-    pdf.line(10, 25, 200,25)
+    #Generate line every 10mm, starting at 20mm until 297mm per page, increasing 10mm
+    for y in range(20, 297, 10):
+        pdf.line(10, y, 200, y)
+
 
     #Footer - LN is different for the first page because of the header
     pdf.ln(265)
@@ -28,5 +31,7 @@ for index, row in df.iterrows():
         pdf.set_font(family="Times", style="I", size=8)
         pdf.set_text_color(180, 180, 180)
         pdf.cell(w=0, h=10, txt=row["Topic"], align="R")
+        for y in range(20, 297, 10):
+            pdf.line(10, y, 200, y)
 
 pdf.output("output.pdf")
