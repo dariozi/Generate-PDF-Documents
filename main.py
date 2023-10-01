82af96ae218f010011d1e261e5e27cc5094d37ed
Dario Z <dario.zignale@yahoo.com>
2023-10-01 09:36:08 +0100

    footer

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from fpdf import FPDF
 import pandas as pd
 
 pdf = FPDF(orientation="P", unit="mm", format="A4")
+pdf.set_auto_page_break(auto=False, margin=0)
 
 df = pd.read_csv("topics.csv")
 
@@ -12,9 +13,20 @@ for index, row in df.iterrows():
     pdf.set_text_color(100,100,100)
     pdf.cell(w=0, h=12, txt=row["Topic"], align="Left", ln=1)
     pdf.line(10, 25, 200,25)
+
+    #Footer - LN is different for the first page because of the header
+    pdf.ln(265)
+    pdf.set_font(family="Times", style="I", size=8)
+    pdf.set_text_color(180,180,180)
+    pdf.cell(w=0, h=10, txt=row["Topic"], align="R")
+
     #we add the -1 because the first page was created above
     for i in range(row["Pages"] - 1):
         pdf.add_page()
-        pdf.cell(w=0, h=12, txt=row["Topic"], align="Left", ln=1)
+        # Footer
+        pdf.ln(277)
+        pdf.set_font(family="Times", style="I", size=8)
+        pdf.set_text_color(180, 180, 180)
+        pdf.cell(w=0, h=10, txt=row["Topic"], align="R")
 
 pdf.output("output.pdf")
